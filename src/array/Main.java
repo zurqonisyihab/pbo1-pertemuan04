b4e0e7775d875879cd4433049aba7e6fde56a604
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-25 01:49:30 -0700

    Tugas Perulangan For

--- src/array/Main.java
+++ src/array/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         double[][] nilaiMahasiswa = new double[5][30];
         double bobot [] = {0.1,0.2,0.3,0.4,};
-        
+
         nilaiMahasiswa[0][0] = 80;
         nilaiMahasiswa[1][0] = 90;
         nilaiMahasiswa[2][0] = 75;
---
+++ src/tugas_perulangan/TugasPerulangan.java
@@ -0,0 +1,13 @@
+package tugas_perulangan;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++){
+            System.out.println("Variabel i saat ini: "+ i);
+        }
+
+        for (int i = 10; i > 0; i--){
+            System.out.println("Variabel i saat ini "+ i);
+        }
+    }
+}
