d16402bf8372c9451bd59ba6500588bde304213b
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-25 01:19:27 -0700

    Array

--- src/array/Main.java
+++ src/array/Main.java
@@ -2,13 +2,9 @@ package array;
 
 public class Main {
     public static void main(String[] args) {
-        String mataKuliah[];
-        mataKuliah = new String[10];
-
-        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
-        mataKuliah[1] = "Visual 2";
-        mataKuliah[2] = "Rekayasa Perangka Lunak";
-        mataKuliah[3] = "Organisasi Komputer";
-        mataKuliah[4] = "Pemrograman Web 2";
+        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
+        System.out.println(nilaiArr.length);
+        System.out.println(nilaiArr[0]);
+        System.out.println(nilaiArr[8]);
     }
 }
