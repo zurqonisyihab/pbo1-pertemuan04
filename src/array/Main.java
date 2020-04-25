0144209a2f3a6b041f1d9dd197126743503f8e50
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-25 01:39:14 -0700

    Multi Dimensional Array

--- src/array/Main.java
+++ src/array/Main.java
@@ -2,9 +2,38 @@ package array;
 
 public class Main {
     public static void main(String[] args) {
-        int nilaiArr[] = {40,55,63,17,22,68,89,97,89};
-        System.out.println(nilaiArr.length);
-        System.out.println(nilaiArr[0]);
-        System.out.println(nilaiArr[8]);
+        double[][] nilaiMahasiswa = new double[5][30];
+        double bobot [] = {0.1,0.2,0.3,0.4,};
+        
+        nilaiMahasiswa[0][0] = 80;
+        nilaiMahasiswa[1][0] = 90;
+        nilaiMahasiswa[2][0] = 75;
+        nilaiMahasiswa[3][0] = 70;
+
+        nilaiMahasiswa[4][0] = nilaiMahasiswa[0][0] * bobot [0] +
+                nilaiMahasiswa[1][0] * bobot [1] +
+                nilaiMahasiswa[2][0] * bobot [2] +
+                nilaiMahasiswa[3][0] * bobot [3];
+
+        System.out.println("Nilai si mahasiswa 1 :"+nilaiMahasiswa[4][0]);
+
+        nilaiMahasiswa[0][0] = 80;
+        nilaiMahasiswa[1][0] = 90;
+        nilaiMahasiswa[2][0] = 75;
+        nilaiMahasiswa[3][0] = 70;
+
+        nilaiMahasiswa[4][1] = 0;
+        for (int i=0 ; i<=3 ; i++) {
+            nilaiMahasiswa [4][1] += nilaiMahasiswa[i][1] * bobot[i];
+        }
+
+        System.out.println("Nilai si mahasiswa 2 : "+nilaiMahasiswa[4][1]);
+
+        double jumlahNilaiAkhir = 0;
+        for (int i=0 ; i<=3 ; i++) {
+            jumlahNilaiAkhir += nilaiMahasiswa[4][i];
+        }
+        System.out.println(jumlahNilaiAkhir);
+
     }
 }
