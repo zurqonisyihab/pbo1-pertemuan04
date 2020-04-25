ae977c7fa676da21d7077abde76dc960e423d818
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-25 01:15:49 -0700

    Belajar Array

---
+++ src/array/Main.java
@@ -0,0 +1,14 @@
+package array;
+
+public class Main {
+    public static void main(String[] args) {
+        String mataKuliah[];
+        mataKuliah = new String[10];
+
+        mataKuliah[0] = "Pemrograman Berbasis Objek 1";
+        mataKuliah[1] = "Visual 2";
+        mataKuliah[2] = "Rekayasa Perangka Lunak";
+        mataKuliah[3] = "Organisasi Komputer";
+        mataKuliah[4] = "Pemrograman Web 2";
+    }
+}
